29ed18de1222cb0d4883e37ee3453a5ee6247f49
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-02-21 15:18:45 -0800

    Rolled back to the previous version and added a sentinel value instead

--- framework/src/scheduler/standalone_scheduler.rs
+++ framework/src/scheduler/standalone_scheduler.rs
@@ -125,21 +125,19 @@ impl StandaloneScheduler {
 
     #[inline]
     fn execute_internal(&mut self, begin: u64) -> u64 {
-        let bool = self.run_q.len()!=self.next_task;
         let time = {
             let task = &mut (&mut self.run_q[self.next_task]);
             task.task.execute();
             let end = utils::rdtsc_unsafe();
             task.cycles += end - begin;
-            if bool {
-                println!("standalone - {:?} cycles", end - begin);
-            }
+            print!("standalone - {:?} cycles", end - begin);
             task.last_run = end;
             end
         };
         let len = self.run_q.len();
         let next = self.next_task + 1;
         if next == len {
+            println!(" 0");
             self.next_task = 0;
             if let Ok(cmd) = self.sched_channel.try_recv() {
                 self.handle_request(cmd);
