8646a31c9f542e7998c68a26a737ba9d0f8f36ce
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-02-24 17:49:06 -0800

    Changed batch size to 200

--- framework/src/operators/receive_batch.rs
+++ framework/src/operators/receive_batch.rs
@@ -23,7 +23,7 @@ impl<T: PacketRx> ReceiveBatch<T> {
 
     pub fn new(queue: T) -> ReceiveBatch<T> {
         ReceiveBatch {
-            parent: PacketBatch::new(200),
+            parent: PacketBatch::new(250),
             queue: queue,
             received: 0,
         }
