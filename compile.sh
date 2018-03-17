f7e29b9a0c16f29863c52e9eb4849935649b8a1d
Ubuntu <ubuntu@ip-172-31-43-151.us-west-2.compute.internal>
2018-03-17 01:09:59 +0000

    modified compile script to automate stuffs

--- compile.sh
+++ compile.sh
@@ -4,3 +4,8 @@ nvcc  -Xcompiler -fPIC -rdc=true -c -o temp.o hello_world.cu
 nvcc -Xcompiler -fPIC -dlink -o hello_world.o temp.o -lcudart
 rm -f libgpu.a
 ar cru libgpu.a hello_world.o temp.o
+
+echo " " >> framework/src/interface/packet.rs
+./build.sh build_fmwk
+./build.sh build_test macswap
+./check.sh
