fa96427e172115a47062520e54e29f674c74632f
Ubuntu <ubuntu@ip-172-31-25-202.us-west-2.compute.internal>
2018-03-19 02:50:40 +0000

    both cpu and gpu running

--- hello_world.cu
+++ hello_world.cu
@@ -7,6 +7,33 @@
 #include<pthread.h>
 #include "nf.cu"
 
+int kunal=5678;
+int Cpu_BatchSize=100;
+int max_size=1024;
+packet_hdrs *hst_hdrs;
+packet_hdrs* dev_hdrs;
+int first=0;
+cudaError_t err = cudaSuccess; 
+size_t size_dev_hdrs;
+
+
+unsigned long long diff(unsigned lo1,unsigned hi1, unsigned lo2, unsigned hi2)
+{
+    unsigned long long int a=( (unsigned long long)lo1)|( ((unsigned long long)hi1)<<32 );
+    unsigned long long int b=( (unsigned long long)lo2)|( ((unsigned long long)hi2)<<32 );
+    return b-a;
+}
+
+typedef struct timer_struct{
+    unsigned cpu_hi1,cpu_hi2,cpu_lo1,cpu_lo2;
+    unsigned gpu_mem_hi1,gpu_mem_hi2,gpu_mem_lo1,gpu_mem_lo2;
+    unsigned gpu_hi1,gpu_hi2,gpu_lo1,gpu_lo2;
+} timer_struct;
+
+timer_struct timers;
+
+//the time measurement code was inspired from https://www.mcs.anl.gov/~kazutomo/rdtsc.html
+
 
 
 typedef struct pthread_struct {
@@ -39,20 +66,84 @@ void *cpu_nf_caller_call(void *arg){
 
 extern "C" {
 void swap_mac_address(GPUMbuf **packetStream, uint64_t size){
-    cudaError_t err = cudaSuccess;
-    cudaDeviceReset();
-    packet_hdrs hst_hdrs[size];
-    packet_hdrs* dev_hdrs;
+  
+  if(size==0)
+    return;
+  printf("%d\n",kunal );
+  kunal++;
+  if(!first){
 
-    pthread_t my_thread;
+    size_dev_hdrs=max_size*sizeof(packet_hdrs);
+    hst_hdrs=(packet_hdrs *)malloc(max_size*sizeof(packet_hdrs));
+    asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+    
+    
+    err=cudaMalloc((void **)&dev_hdrs, size_dev_hdrs);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+    first=1;
+    asm volatile ("cpuid\n\t"
+  "rdtsc\n\t"
+  "mov %%edx, %0\n\t"
+  "mov %%eax, %1\n\t"
+  : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
+  :: "%rax", "%rbx", "%rcx", "%rdx");
+    printf("CUDA MALLOC::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
+
+  }
+    printf("%llu\n",size );
+  asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.cpu_hi1), "=r" (timers.cpu_lo1)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+   pthread_t my_thread;
     pthread_struct pthread_Args;
     pthread_Args.packetStream=packetStream;
-    pthread_Args.size=size;
-
+    pthread_Args.size=200;
+ 
     pthread_create(&my_thread, NULL, cpu_nf_caller_call, &pthread_Args); 
     pthread_join(my_thread, NULL);
+
+  asm volatile (
+  "rdtsc\n\t"
+  "mov %%edx, %0\n\t"
+  "mov %%eax, %1\n\t"
+  : "=r" (timers.cpu_hi2), "=r" (timers.cpu_lo2)
+  :: "%rax", "%rbx", "%rcx", "%rdx");
+  unsigned long long int cpu_time=diff(timers.cpu_lo1,timers.cpu_hi1,timers.cpu_lo2,timers.cpu_hi2);
+    // printf("CPU::%llu\n", );
+
+  asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_mem_hi1), "=r" (timers.gpu_mem_lo1)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+
+    asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+
+    
+    // cudaDeviceReset();
+
+   
+
     // cpu_nf_caller_call(packetStream,size);
-    for(int i=0;i<size;i++)
+    for(int i=200;i<size;i++)
     {
         GPUMbuf mbuf=*(packetStream[i]);
         uint8_t* buf=mbuf.buf_addr+mbuf.data_off;
@@ -70,22 +161,48 @@ void swap_mac_address(GPUMbuf **packetStream, uint64_t size){
         // memcpy(&ip_addr.s_addr, &hst_hdrs[i].ipHdr.dst_ip, 4);
         // printf("\nThe src IP address is %s\n", inet_ntoa(ip_addr));
     }
-    size_t size_dev_hdrs=size*sizeof(packet_hdrs);
-    err = cudaMalloc((void **)&dev_hdrs, size_dev_hdrs);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device vector (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
+    asm volatile ("cpuid\n\t"
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+      printf("CPU Copy time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
+    
+
+
+
+
+     asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
     err = cudaMemcpy(dev_hdrs, hst_hdrs,size_dev_hdrs, cudaMemcpyHostToDevice);
     if (err != cudaSuccess)
     {
         fprintf(stderr, "Failed to copy to GPU device (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-    err=cudaDeviceSynchronize();
+    // err=cudaDeviceSynchronize();
+    asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+      printf("GPU Copy time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
+    // size_t size_dev_hdrs=size*sizeof(packet_hdrs);
 
-    // gpu_kernel_call(dev_hdrs, size);
+    asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+
+    gpu_kernel_call(dev_hdrs, size);
     
     
     err=cudaDeviceSynchronize();
@@ -95,20 +212,69 @@ void swap_mac_address(GPUMbuf **packetStream, uint64_t size){
         exit(EXIT_FAILURE);
     }
 
+        asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+      printf("GPU Run time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
+
+      asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
     err = cudaMemcpy(hst_hdrs, dev_hdrs, size_dev_hdrs, cudaMemcpyDeviceToHost);
     if (err != cudaSuccess){
          fprintf(stderr, "Failed to copy vectorC from device to host (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
+    // cudaFree(dev_hdrs);
+     asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+        printf("GPU copy back time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
 
-     for(int i=0;i<size;i++)
+
+
+    asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+    for(int i=0;i<size;i++)
     {
         GPUMbuf mbuf=*(packetStream[i]);
         uint8_t* buf=mbuf.buf_addr+mbuf.data_off;
         // memcpy((uint8_t*)&hst_hdrs[i],buf,sizeof(packet_hdrs));
         memcpy(buf,(uint8_t*)&hst_hdrs[i],sizeof(packet_hdrs));
     }
+    asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+        printf("CPU Copy back time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
+
+    asm volatile (
+      "rdtsc\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t"
+      : "=r" (timers.gpu_mem_hi2), "=r" (timers.gpu_mem_lo2)
+      :: "%rax", "%rbx", "%rcx", "%rdx");
+    unsigned long long int gpu_time=diff(timers.gpu_mem_lo1,timers.gpu_mem_hi1,timers.gpu_mem_lo2,timers.gpu_mem_hi2);
+    printf("GPU::%llu\n", gpu_time);
+    printf("CPU::%llu\n",cpu_time );
+    printf("Tot::%llu,\n",cpu_time+gpu_time);
 }
+
 
 }
 
--- nf.cu
+++ nf.cu
@@ -4,14 +4,38 @@ __global__ void mac_swap_kernel(packet_hdrs *hst_hdrs, uint64_t size){
 	int tid=threadIdx.x;
 	if(tid<size){
         uint8_t tmp[6];
+        // memcpy(&tmp,&hst_hdrs[tid].ethHdr.src_address,6);
+        // memcpy(&hst_hdrs[tid].ethHdr.src_address,&hst_hdrs[tid].ethHdr.dst_address,6);
+        // memcpy(&hst_hdrs[tid].ethHdr.dst_address,&tmp,6);
         memcpy(&tmp,&hst_hdrs[tid].ethHdr.src_address,6);
         memcpy(&hst_hdrs[tid].ethHdr.src_address,&hst_hdrs[tid].ethHdr.dst_address,6);
         memcpy(&hst_hdrs[tid].ethHdr.dst_address,&tmp,6);
+        /*
+        memcpy(&tmp,&hst_hdrs[tid].ethHdr.src_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.src_address,&hst_hdrs[tid].ethHdr.dst_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.dst_address,&tmp,6);
+        memcpy(&tmp,&hst_hdrs[tid].ethHdr.src_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.src_address,&hst_hdrs[tid].ethHdr.dst_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.dst_address,&tmp,6);
+        memcpy(&tmp,&hst_hdrs[tid].ethHdr.src_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.src_address,&hst_hdrs[tid].ethHdr.dst_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.dst_address,&tmp,6);
+        memcpy(&tmp,&hst_hdrs[tid].ethHdr.src_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.src_address,&hst_hdrs[tid].ethHdr.dst_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.dst_address,&tmp,6);
+        memcpy(&tmp,&hst_hdrs[tid].ethHdr.src_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.src_address,&hst_hdrs[tid].ethHdr.dst_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.dst_address,&tmp,6);
+        memcpy(&tmp,&hst_hdrs[tid].ethHdr.src_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.src_address,&hst_hdrs[tid].ethHdr.dst_address,6);
+        memcpy(&hst_hdrs[tid].ethHdr.dst_address,&tmp,6);
+        */
 	}
 }
 
 void gpu_kernel_call(packet_hdrs *dev_hdrs,uint64_t size){
-    mac_swap_kernel<<<1,size>>>(dev_hdrs, size);
+    int numblocks=(size/128)+1;
+    mac_swap_kernel<<<numblocks,128>>>(dev_hdrs, size);
 }
 
 void cpu_nf_call(packet_hdrs *pack_hdr)
@@ -23,4 +47,26 @@ void cpu_nf_call(packet_hdrs *pack_hdr)
     memcpy(&tmp,pack_hdr->ethHdr.src_address,6);
     memcpy(pack_hdr->ethHdr.src_address,pack_hdr->ethHdr.dst_address,6);
     memcpy(pack_hdr->ethHdr.dst_address,&tmp,6);
+    /*
+    memcpy(&tmp,pack_hdr->ethHdr.src_address,6);
+    memcpy(pack_hdr->ethHdr.src_address,pack_hdr->ethHdr.dst_address,6);
+    memcpy(pack_hdr->ethHdr.dst_address,&tmp,6);
+    memcpy(&tmp,pack_hdr->ethHdr.src_address,6);
+    memcpy(pack_hdr->ethHdr.src_address,pack_hdr->ethHdr.dst_address,6);
+    memcpy(pack_hdr->ethHdr.dst_address,&tmp,6);
+    memcpy(&tmp,pack_hdr->ethHdr.src_address,6);
+    memcpy(pack_hdr->ethHdr.src_address,pack_hdr->ethHdr.dst_address,6);
+    memcpy(pack_hdr->ethHdr.dst_address,&tmp,6);
+    memcpy(&tmp,pack_hdr->ethHdr.src_address,6);
+    memcpy(pack_hdr->ethHdr.src_address,pack_hdr->ethHdr.dst_address,6);
+    memcpy(pack_hdr->ethHdr.dst_address,&tmp,6);
+    memcpy(&tmp,pack_hdr->ethHdr.src_address,6);
+    memcpy(pack_hdr->ethHdr.src_address,pack_hdr->ethHdr.dst_address,6);
+    memcpy(pack_hdr->ethHdr.dst_address,&tmp,6);
+    memcpy(&tmp,pack_hdr->ethHdr.src_address,6);
+    memcpy(pack_hdr->ethHdr.src_address,pack_hdr->ethHdr.dst_address,6);
+    memcpy(pack_hdr->ethHdr.dst_address,&tmp,6);
+    memcpy(&tmp,pack_hdr->ethHdr.src_address,6);
+    memcpy(pack_hdr->ethHdr.src_address,pack_hdr->ethHdr.dst_address,6);
+    memcpy(pack_hdr->ethHdr.dst_address,&tmp,6);*/
 }
