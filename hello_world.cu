dc611df4864f8833d8e71043c60006a25157b457
Ubuntu <soanand14@gmail.com>
2018-03-20 21:25:12 +0000

    Cleanup

--- hello_world.cu
+++ hello_world.cu
@@ -7,8 +7,12 @@
 #include<pthread.h>
 #include "nf.cu"
 
+#define timeIO 1
+#define RunBoth 1
+
 int kunal=5678;
-int Cpu_BatchSize=100;
+int Cpu_BatchSize;
+// int GPU_BatchSize;
 int max_size=512;
 packet_hdrs *hst_hdrs;
 packet_hdrs* dev_hdrs;
@@ -61,265 +65,183 @@ void *cpu_nf_caller_call(void *arg){
     return NULL;
 }
 
-
-
-
-extern "C" {
-void swap_mac_address(GPUMbuf **packetStream, uint64_t size){
-  
-  if(size==0)
-    return;
-  printf("%d\n",kunal );
-  kunal++;
-  if(!first){
-
-    size_dev_hdrs=max_size*sizeof(packet_hdrs);
-    hst_hdrs=(packet_hdrs *)malloc(max_size*sizeof(packet_hdrs));
+void GPU_startTime()
+{
     asm volatile (
       "rdtsc\n\t"
       "mov %%edx, %0\n\t"
       "mov %%eax, %1\n\t"
       : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
       :: "%rax", "%rbx", "%rcx", "%rdx");
-    
-    
-    err=cudaMalloc((void **)&dev_hdrs, size_dev_hdrs);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device vector (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-    first=1;
-    asm volatile ("cpuid\n\t"
+
+}
+
+void GPU_endTime()
+{
+  asm volatile ("cpuid\n\t"
   "rdtsc\n\t"
   "mov %%edx, %0\n\t"
   "mov %%eax, %1\n\t"
   : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
   :: "%rax", "%rbx", "%rcx", "%rdx");
-    printf("CUDA MALLOC::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
+}
 
-  }
-
-
-
-
-
-
-
-
-
-    printf("%llu\n",size );
-
-
-
-asm volatile (
+void CPU_startTime()
+{
+  asm volatile (
       "rdtsc\n\t"
       "mov %%edx, %0\n\t"
       "mov %%eax, %1\n\t"
       : "=r" (timers.cpu_hi1), "=r" (timers.cpu_lo1)
       :: "%rax", "%rbx", "%rcx", "%rdx");
-   pthread_t my_thread;
-    pthread_struct pthread_Args;
-    pthread_Args.packetStream=packetStream;
-    pthread_Args.size=size;
- 
-    pthread_create(&my_thread, NULL, cpu_nf_caller_call, &pthread_Args); 
-   pthread_join(my_thread, NULL);
+}
 
-    asm volatile (
+void CPU_endTime()
+{
+  asm volatile (
   "rdtsc\n\t"
   "mov %%edx, %0\n\t"
   "mov %%eax, %1\n\t"
   : "=r" (timers.cpu_hi2), "=r" (timers.cpu_lo2)
   :: "%rax", "%rbx", "%rcx", "%rdx");
-  unsigned long long int cpu_time=diff(timers.cpu_lo1,timers.cpu_hi1,timers.cpu_lo2,timers.cpu_hi2);
+}
 
-
-
-
-
-
+void GPU_Tot_startTime()
+{
   asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.cpu_hi1), "=r" (timers.cpu_lo1)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
-   pthread_t my_thread2;
+        "rdtsc\n\t"
+        "mov %%edx, %0\n\t"
+        "mov %%eax, %1\n\t"
+        : "=r" (timers.gpu_mem_hi1), "=r" (timers.gpu_mem_lo1)
+        :: "%rax", "%rbx", "%rcx", "%rdx");
+
+}
+
+void GPU_Tot_endTime()
+{
+        asm volatile (
+        "rdtsc\n\t"
+        "mov %%edx, %0\n\t"
+        "mov %%eax, %1\n\t"
+        : "=r" (timers.gpu_mem_hi2), "=r" (timers.gpu_mem_lo2)
+        :: "%rax", "%rbx", "%rcx", "%rdx");
+
+}
+extern "C" {
+void swap_mac_address(GPUMbuf **packetStream, uint64_t size){
+  if(size==0)
+    return;
+
+  if(!first){
+    size_dev_hdrs=max_size*sizeof(packet_hdrs);
+    hst_hdrs=(packet_hdrs *)malloc(max_size*sizeof(packet_hdrs));
+    GPU_startTime();
+    err=cudaMalloc((void **)&dev_hdrs, size_dev_hdrs);
+    if (err != cudaSuccess){
+        fprintf(stderr, "Failed to allocate device vector (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+    first=1;
+    void GPU_endTime();  
+    if(timeIO)
+      printf("CUDA MALLOC::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
+  }
+  unsigned long long int cpu_time
+
+  if(timeIO)
+    printf("%llu\n",size );
+
+  if(RunBoth){
+    CPU_startTime();
+
+    pthread_t my_thread;
+    pthread_struct pthread_Args;
+    pthread_Args.packetStream=packetStream;
+    pthread_Args.size=size;
+    pthread_create(&my_thread, NULL, cpu_nf_caller_call, &pthread_Args); 
+    pthread_join(my_thread, NULL);
+
+    CPU_endTime()
+    cpu_time=diff(timers.cpu_lo1,timers.cpu_hi1,timers.cpu_lo2,timers.cpu_hi2);
+  }
+  else{
+    CPU_startTime();
+    pthread_t my_thread2;
     pthread_struct pthread_Args2;
     pthread_Args2.packetStream=packetStream;
-    pthread_Args2.size=200;
- 
-    pthread_create(&my_thread2, NULL, cpu_nf_caller_call, &pthread_Args2); 
+    pthread_Args2.size=Cpu_BatchSize;
    
-
-  
-    // printf("CPU::%llu\n", );
-
-  asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_mem_hi1), "=r" (timers.gpu_mem_lo1)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
-
-    asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
+    pthread_create(&my_thread2, NULL, cpu_nf_caller_call, &pthread_Args2); 
 
     
-    // cudaDeviceReset();
-
-   
-
-    // cpu_nf_caller_call(packetStream,size);
-    for(int i=200;i<size;i++)
-    {
+    GPU_Tot_startTime();
+    GPU_startTime();
+    for(int i=Cpu_BatchSize;i<size;i++){
         GPUMbuf mbuf=*(packetStream[i]);
         uint8_t* buf=mbuf.buf_addr+mbuf.data_off;
         memcpy((uint8_t*)&hst_hdrs[i],buf,sizeof(packet_hdrs));
-        // for(int j=0;j<6;j++)
-        //     printf("%02x::", hst_hdrs[i].ethHdr.dst_address[j]);
-        // printf("<---->");
-        // for(int j=0;j<6;j++)
-        //     printf("%02x::", hst_hdrs[i].ethHdr.src_address[j]);
-        // printf("\n");
-        // struct in_addr ip_addr;
-        // ip_addr.s_addr=;
-        //  memcpy(&ip_addr.s_addr, &hst_hdrs[i].ipHdr.src_ip, 4);
-        // printf("\nThe src IP address is %s\n", inet_ntoa(ip_addr));
-        // memcpy(&ip_addr.s_addr, &hst_hdrs[i].ipHdr.dst_ip, 4);
-        // printf("\nThe src IP address is %s\n", inet_ntoa(ip_addr));
     }
-    asm volatile ("cpuid\n\t"
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
+    GPU_endTime();
+    if(timeIO)
       printf("CPU Copy time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
-    
-
-
-
-
-     asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
+      
+    GPU_startTime();
     err = cudaMemcpy(dev_hdrs, hst_hdrs,size_dev_hdrs, cudaMemcpyHostToDevice);
     if (err != cudaSuccess)
     {
         fprintf(stderr, "Failed to copy to GPU device (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-    // err=cudaDeviceSynchronize();
-    asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
+    GPU_endTime();
+
+    if(timeIO)
       printf("GPU Copy time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
-    // size_t size_dev_hdrs=size*sizeof(packet_hdrs);
 
-    asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
-
-    gpu_kernel_call(dev_hdrs, size);
-    
-    
+    GPU_startTime();
+    gpu_kernel_call(dev_hdrs, size-Cpu_BatchSize);  
     err=cudaDeviceSynchronize();
     err = cudaGetLastError();
     if (err != cudaSuccess){
         fprintf(stderr, "Failed to launch mac_swap_kernel kernel (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-
-        asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
+    GPU_endTime();
+    if(timeIO)
       printf("GPU Run time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
-
-      asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
+    
+    GPU_startTime();
     err = cudaMemcpy(hst_hdrs, dev_hdrs, size_dev_hdrs, cudaMemcpyDeviceToHost);
     if (err != cudaSuccess){
          fprintf(stderr, "Failed to copy vectorC from device to host (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-    // cudaFree(dev_hdrs);
-     asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
-        printf("GPU copy back time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
+      // cudaFree(dev_hdrs);
+    GPU_endTime();
+    if(timeIO)
+      printf("GPU copy back time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
 
-
-
-    asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_hi1), "=r" (timers.gpu_lo1)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
-    for(int i=0;i<size;i++)
-    {
+    GPU_startTime();
+    for(int i=0;i<size;i++){
         GPUMbuf mbuf=*(packetStream[i]);
         uint8_t* buf=mbuf.buf_addr+mbuf.data_off;
         // memcpy((uint8_t*)&hst_hdrs[i],buf,sizeof(packet_hdrs));
         memcpy(buf,(uint8_t*)&hst_hdrs[i],sizeof(packet_hdrs));
     }
-    asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_hi2), "=r" (timers.gpu_lo2)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
-        printf("CPU Copy back time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
-
-    asm volatile (
-      "rdtsc\n\t"
-      "mov %%edx, %0\n\t"
-      "mov %%eax, %1\n\t"
-      : "=r" (timers.gpu_mem_hi2), "=r" (timers.gpu_mem_lo2)
-      :: "%rax", "%rbx", "%rcx", "%rdx");
-
-
-
+    GPU_endTime();
+    if(timeIO)
+      printf("CPU Copy back time::%llu\n", diff(timers.gpu_lo1,timers.gpu_hi1,timers.gpu_lo2,timers.gpu_hi2));
+    GPU_Tot_endTime();
     pthread_join(my_thread2, NULL);
+    CPU_endTime();
+    unsigned long long int hyb_time=diff(timers.cpu_lo1,timers.cpu_hi1,timers.cpu_lo2,timers.cpu_hi2);
 
-    asm volatile (
-  "rdtsc\n\t"
-  "mov %%edx, %0\n\t"
-  "mov %%eax, %1\n\t"
-  : "=r" (timers.cpu_hi2), "=r" (timers.cpu_lo2)
-  :: "%rax", "%rbx", "%rcx", "%rdx");
-
-  unsigned long long int hyb_time=diff(timers.cpu_lo1,timers.cpu_hi1,timers.cpu_lo2,timers.cpu_hi2);
-     
     unsigned long long int gpu_time=diff(timers.gpu_mem_lo1,timers.gpu_mem_hi1,timers.gpu_mem_lo2,timers.gpu_mem_hi2);
     printf("GPU::%llu\n", gpu_time);
     printf("CPU::%llu\n",cpu_time );
     printf("HYB::%llu,\n",hyb_time);
+
+  }
+  
 }
 
 
